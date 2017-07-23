8661bb2ee1c69d522ec123f1ccc98f28f78036cd
anagharumade <anagharumade.8@gmail.com>
2017-07-23 19:35:12 -0400

    changes

--- exp1.py
+++ exp1.py
@@ -1,5 +1,11 @@
+from fn_dataframes import run
 import matplotlib.pyplot as plt
 
-plt.plot([1,2,3],[4,5,1])
+#df = run('April_2008.csv')
+#plt.plot([1,2,3],[4,5,1])
+plt.plot(run('April_2008.csv'))
 
+plt.xlabel('Query Label')
+plt.ylabel('Rank')
+plt.title('April 2008\ncsv file')
 plt.show()
--- fn_dataframes.py
+++ fn_dataframes.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
 def run(path):
-    aa = pd.read_csv(path)
-    return (aa)
+    df = pd.read_csv(path)
+    #df = df[['query_label','rank']]
+    sr = df[[0]]
+    return (sr)
 
 run('April_2008.csv')
