7b94d4768c2dc43295f65e2442762e0783242b64
getmykhan <mkhan7@stevens.edu>
2017-07-23 19:41:47 -0400

    dataframes

--- fn_dataframes.py
+++ fn_dataframes.py
@@ -1,15 +1,6 @@
 import pandas as pd
 
 def run(path):
-<<<<<<< HEAD
-    df = pd.read_csv(path)
-    #df = df[['query_label','rank']]
-    sr = df[[0]]
-    return (sr)
-
-run('April_2008.csv')
-=======
-    def run(path):
     df =  pd.read_csv(path)
     #print(aa['query_label'],['rank'])
     #df1 = df[['distance', 'rank']]
@@ -17,7 +8,6 @@ run('April_2008.csv')
     sr = df[[3]]
     sr2 = df[[2]]
     print(sr, sr2)
-    #plt.plot(sr, sr2)
+    plt.plot(sr, sr2)
     return (sr, sr2)
 run('April_2008.csv')
->>>>>>> 861c9db176c7a28500891f1bcc9c1e9fa49f7d05
