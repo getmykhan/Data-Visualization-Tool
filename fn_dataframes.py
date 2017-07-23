861c9db176c7a28500891f1bcc9c1e9fa49f7d05
getmykhan <mkhan7@stevens.edu>
2017-07-23 19:33:21 -0400

    minor yweaks

--- fn_dataframes.py
+++ fn_dataframes.py
@@ -1,7 +1,14 @@
 import pandas as pd
 
 def run(path):
-    aa = pd.read_csv(path)
-    return (aa)
-
+    def run(path):
+    df =  pd.read_csv(path)
+    #print(aa['query_label'],['rank'])
+    #df1 = df[['distance', 'rank']]
+    #df1 = df.ix[0:,2:]
+    sr = df[[3]]
+    sr2 = df[[2]]
+    print(sr, sr2)
+    #plt.plot(sr, sr2)
+    return (sr, sr2)
 run('April_2008.csv')
