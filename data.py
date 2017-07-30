e34eca8708af641e357885333f2fd27581d40056
anagharumade <anagharumade.8@gmail.com>
2017-07-29 22:35:26 -0400

    User input added to select specific column

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import sys
 
 def PullDataCsv(path):
     df =  pd.read_csv(path)                 #Reading CSV file
@@ -12,12 +13,17 @@ def PullDataCsv(path):
         #(dfcol + str(i)) = dfnum[column]
         i = i+1
 
+    dfcolarr = {}
+    j=0
+    while j < len(col_list):
+        dfcolarr[j] = dfcol[col_list[j]].values
+        j=j+1
     #dfcol1 = dfnum.ix[:,0]
     #dfcol2 = dfnum.ix[:,1]
-    dfcol1 = dfcol[0].values                #Create array of values in the dictionary
-    dfcol2 = dfcol[1].values
+    #dfcol1 = dfcol[1].values                #Create array of values in the dictionary
+    #dfcol2 = dfcol[2].values
 
-    csvplot(dfcol1,dfcol2,path)
+    csvplot(dfcolarr[0],dfcolarr[1],path)
 
 def csvplot(dfcol1,dfcol2,path):
 
@@ -26,4 +32,15 @@ def csvplot(dfcol1,dfcol2,path):
     plt.show()
 
 if __name__ == '__main__':
+    col_list = []
+    while True:
+        cols = input("Enter the column: ")
+        col_list.append(int(cols))
+        exit = input("Done?: ")
+        if exit == "yes":
+            break
+        else:
+            continue
+
+    print (col_list[1])
     PullDataCsv('April_2008.csv')
